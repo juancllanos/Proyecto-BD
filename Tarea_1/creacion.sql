d881e9ee18ebdfb86ade5b71b48a8b7ec34a9571
juancllanos <juanc.llanos@urosario.edu.co>
2019-04-27 15:59:21 -0500

    Añadí la parte de SQL de la tarea 1

---
+++ Tarea_1/creacion.sql
@@ -0,0 +1,46 @@
+create table usuario
+( nickname varchar(15),
+ nombre varchar(20),
+ contraseña varchar(15),
+ primary key(nickname)
+);
+
+create table canciones
+( id varchar (7),
+ nombre varchar(20),
+ autor varchar(20),
+ genero varchar(15),
+  primary key(id)
+);
+
+create table playlist
+( id varchar (7),
+ nombre varchar(30),
+  primary key(id)
+ );
+
+
+create table tiene
+( usuario varchar (15),
+ id_playlist varchar(7),
+  primary key(usuario,id_playlist),
+ foreign key (usuario) references usuario(nickname),
+ foreign key (id_playlist) references playlist(id)
+ );
+
+create table gusta 
+( usuario varchar (15),
+ id_cancion varchar(7),
+  primary key(usuario,id_cancion),
+  foreign key (usuario) references usuario(nickname),
+ foreign key (id_cancion) references canciones(id)
+);
+
+
+create table contiene
+( id_playlist varchar (7),
+ id_cancion varchar(7),
+  primary key(id_cancion,id_playlist),
+  foreign key (id_cancion) references canciones(id),
+ foreign key (id_playlist) references playlist(id)
+);
